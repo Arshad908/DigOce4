91d55a5587c375b29b5530e1ff4f8c0beb914c2d
Arshad908 <46696775+Arshad908@users.noreply.github.com>
2019-01-22 19:36:16 +0530

    View Update
    -

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,15 +79,11 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Welcome
+                   This is update
                 </div>
-
+                    
                 <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="https://www.fhlpaytrack.com/login">My Invoice</a>
                 </div>
             </div>
         </div>
