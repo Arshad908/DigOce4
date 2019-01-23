49ce75fd73138aa681525dbab04098bc1db3ac0f
Arshad_Cloud <adleen786@gmail.com>
2019-01-22 18:12:50 -0800

    Commit_two

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                   Hi! I From
+                   Hi! I From Rajagiriya
                 </div>
                     
                 <div class="links">
